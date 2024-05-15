eada1e9c96fd527a36337c584a759d71c90c0576
brachi-f <braf5866@gmail.com>
2024-05-15 20:21:40 +0300

    add JWT

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -3,9 +3,8 @@ import UserController from "../Controllers/usersController.js"
 
 const UserRouter = express.Router()
 
-UserRouter.get('/', UserController.getList)
+// UserRouter.get('/', UserController.getList)
 UserRouter.get('/:id', UserController.getUserById)
-UserRouter.post('/', UserController.addUser)
 UserRouter.put('/:id', UserController.updateUser)
 UserRouter.delete('/:id', UserController.deleteUser)
 UserRouter.get('/:id/links', UserController.getLinks)
